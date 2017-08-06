c39aab53f1b8e3d86769a4e99b11f5470b914d31
John Holdsworth <github@johnholdsworth.com>
2017-08-06 16:11:46 +0100

    Updated for Version 2

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "java_lang",
     dependencies: [
-        Package(url: "https://github.com/SwiftJava/java_swift.git", versions: Version(2,0,0)..<Version(3,0,0)),
+        .Package(url: "https://github.com/SwiftJava/java_swift.git", versions: Version(2,0,0)..<Version(3,0,0)),
 //        Package(url: "https://github.com/SwiftJava/java_util.git", versions: Version(1,0,0)..<Version(2,0,0)),
 //        Package(url: "https://github.com/SwiftJava/java_io.git", versions: Version(1,0,0)..<Version(2,0,0)),
 //        Package(url: "https://github.com/SwiftJava/java_net.git", versions: Version(1,0,0)..<Version(2,0,0)),
